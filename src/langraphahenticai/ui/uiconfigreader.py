d1d45fca230e507e5fe874b2ec7b42f994876417
Sudipta Ghosh <sudiptakr@gmail.com>
2025-10-08 03:49:46 +0530

    Add files via upload

--- src/langraphahenticai/ui/uiconfigreader.py
+++ src/langraphahenticai/ui/uiconfigreader.py
@@ -2,7 +2,7 @@ from configparser import ConfigParser
 
 
 class Config:
-    def __init__(self,config_file="./src/langgraphagenticai/ui/uiconfig.ini"):
+    def __init__(self,config_file="./src/langraphahenticai/ui/uiconfig.ini"):
         self.config=ConfigParser()
         self.config.read(config_file)
 
